4f69c8c77ed25f3e58bf2001e513858d26c8639e
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-11 20:45:59 +0530

    Add files via upload

--- Day-3/Reverse.java
+++ Day-3/Reverse.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Reverse {
+    public static void main(String[] args) {
+        System.out.println("enter number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int rem;
+        int rev=0;
+        while(num!=0)
+        {
+            rem=num%10;
 
+            rev=rev*10+rem;
+            num=num/10;
+            if(rem==0)
+            {
+                continue;}
+
+        }System.out.println(rev);
+    }
+}
