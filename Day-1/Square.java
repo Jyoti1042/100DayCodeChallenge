a04716a90d1767c671b29dd1f64ae81d995ee27f
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-11 20:40:43 +0530

    Add files via upload

--- Day-1/Square.java
+++ Day-1/Square.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args) {
+        System.out.println("Enter side of square");
+        Scanner sc=new Scanner(System.in);
+        double side=sc.nextDouble();
+        double area=side*side;
+        double perimeter=4*side;
+        System.out.println("area of square is "+area);
+        System.out.println("perimeter of square is "+perimeter);
+    }
+}
