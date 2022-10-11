424173a0288aa39b081e653f606bd6d4bf117a93
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-11 20:52:20 +0530

    Add files via upload

--- Day-5/Convert.java
+++ Day-5/Convert.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class Convert {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        System.out.println("enter Starting no");
+        int s=sc.nextInt();
+        System.out.println("enter ending no");
+        int e=sc.nextInt();
+        System.out.println("enter the gap no");
+        int w=sc.nextInt();
+        int f;
+        System.out.println("...............................");
+        while(s<=e)
+        { 
+            f=(5*(s-32))/9;
+            System.out.println(s+" "+f);
+            s=s+w;
+        }}}
+    
