4aaa112139a9d9de1d7cdc4538757f7c64e8eda3
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-11 20:54:42 +0530

    Add files via upload

--- Day-6/Palindrome.java
+++ Day-6/Palindrome.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("enter no");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    int rem;
+    int rev=0;
+    int t=n;
+    
+    while(t!=0)
+    {
+        rem=t%10;
+        rev=rev*10+rem;
+        t=t/10;
+    }
+    if(n==rev)
+    {
+        System.out.println(" palindrome no");
+    }else{
+        System.out.println("Not a palindrome no");
+    }
 
+}
+    }
