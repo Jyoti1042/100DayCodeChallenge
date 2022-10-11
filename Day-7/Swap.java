728affba25459b234929772356231a83e88326e4
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-11 20:57:17 +0530

    Add files via upload

--- Day-7/Swap.java
+++ Day-7/Swap.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Swap {  
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter x");
+    int x=sc.nextInt();
+   System.out.println("enter y");
+    int y=sc.nextInt();
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    System.out.println("After swapping value of x is "+ x + " and y is " +y);
+}}
 
