a121e8609fa9be4985a7c203270e4a2d76692838
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-16 17:21:25 +0530

    Create BubbleSort.java

---
+++ Day-14/Bubble Sort/BubbleSort.java
@@ -0,0 +1,20 @@
+#BubbleSort Using Java.
+Bubble sort is a simple sorting algorithm.
+This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order.
+This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
+Test Case:
+Input:
+Enter number of elements:5
+Enter Elements:
+12
+56
+23
+90
+11
+Output:
+Sorted list is:
+11
+12
+23
+56
+90
