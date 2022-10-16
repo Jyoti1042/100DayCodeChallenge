aedcbeebcb0eb2826caec293b4a1dd36f888e898
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-10-16 17:21:53 +0530

    Add files via upload

--- Day-14/Bubble Sort/BubbleSort.java
+++ Day-14/Bubble Sort/BubbleSort.java
@@ -1,20 +1,37 @@
-#BubbleSort Using Java.
-Bubble sort is a simple sorting algorithm.
-This sorting algorithm is comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if they are not in order.
-This algorithm is not suitable for large data sets as its average and worst case complexity are of Ο(n2) where n is the number of items.
-Test Case:
-Input:
-Enter number of elements:5
-Enter Elements:
-12
-56
-23
-90
-11
-Output:
-Sorted list is:
-11
-12
-23
-56
-90
+import java.util.Scanner;
+public class BubbleSort {
+public static void main(String[] args) {
+    System.out.println("Enter number of elements");
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    System.out.println("Enter elements");
+    int a[]=new int[n];
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        a[i]=sc.nextInt();
+    }
+    for(int i=0;i<a.length;i++)
+    {
+       int flag=0;
+    for(int j=0;j<a.length-1-i;j++)
+    {
+        if(a[j]>a[j+1])
+        {
+            temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+            flag=1;
+        }  
+    }
+    if(flag==0)
+    {
+        break;
+    }
+    }System.out.println("Sorted list is");
+    for(int i=0;i<a.length;i++)
+    {
+        System.out.println(a[i]+" ");
+    }
+} 
+}
