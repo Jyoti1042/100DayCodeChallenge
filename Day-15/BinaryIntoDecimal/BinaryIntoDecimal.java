56bc0f7072757e0e17fd9201cdbef3d3ba8d34a1
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-11-02 19:40:14 +0530

    Add files via upload

--- Day-15/BinaryIntoDecimal/BinaryIntoDecimal.java
+++ Day-15/BinaryIntoDecimal/BinaryIntoDecimal.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+public class BinaryIntoDecimal {
+    public static void main(String[] args) {
+        System.out.println("enter binary number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int rem;
+        int dec=0;
+        int i=1;
+        while(num!=0)
+        {
+            rem=num%10;
+            dec=dec+rem*i;
+            i=i*2;
+            num=num/10;
+        }System.out.println(dec);
+    }
+}
