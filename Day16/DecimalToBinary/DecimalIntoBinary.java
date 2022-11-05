1d7d2c78e97c926485541f949ebf9bc6b3f47528
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-11-05 11:53:43 +0530

    Add files via upload

---
+++ Day16/DecimalToBinary/DecimalIntoBinary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class DecimalIntoBinary {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int arr[]=new int[20];
+        int index=0;
+        if(num==0)
+        {
+            System.out.println(0);
+        }
+        while(num>0)
+        {
+            arr[index]=num%2;
+            index++;
+            num=num/2;
+            
+        }
+        for(int i=index-1;i>=0;i--)
+        {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
