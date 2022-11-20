d55f56b251dfb616e18b80d2ad5da386c716a996
Jyoti <114691373+Jyoti1042@users.noreply.github.com>
2022-11-20 22:58:30 +0530

    Create Overriding.java

---
+++ Day-24/Method overriding/Overriding.java
@@ -0,0 +1,45 @@
+Definition:If subclass (child class) has the same method as declared in the parent class, it is known as method overriding in Java.
+
+In other words, If a subclass provides the specific implementation of the method that has been declared by one of its parent class,
+it is known as method overriding.
+Test:
+Output:
+Generic Sports
+Each team has n players in Generic Sports
+Soccer Class
+Each team has 11 players in Soccer Class
+......................................................................................................................................................................
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println("Each team has 11 players in " + getName());
+    }
+}
+
+public class Solution{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
